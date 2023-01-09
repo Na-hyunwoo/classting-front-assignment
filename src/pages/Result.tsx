d847e0bb03eb73e49e80cc5e219bfe2281097d69
hw <dusdjeks@naver.com>
2023-01-09 20:24:35 +0900

    Add result page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,10 +1,32 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { settingState } from "../store";
+import { SettingType } from "../types";
 
 const Result = (): ReactElement => {
+
+  const score = window.localStorage.getItem("score");
+
+  const setSettings = useSetRecoilState<SettingType>(settingState);
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      Result
-    </div>
+    <Box mt={30}>
+      <Typography 
+        variant="h3" 
+        fontWeight="bold" 
+        mb={3}
+      >Result: {score}</Typography>
+      <Button onClick={handleClick} variant="outlined">Back To Home</Button>
+    </Box>
   );
 };
 
