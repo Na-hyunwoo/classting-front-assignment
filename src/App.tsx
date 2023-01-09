2c69e84d048df188a8d0e80a4e2debb34307bd25
hw <dusdjeks@naver.com>
2023-01-09 17:30:03 +0900

    Set initial config like react-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Box, Container } from '@mui/system';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Question, Result, Setting } from "./pages";
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route path="/" element={<Setting />} />
+            <Route path="/question" element={<Question />} />
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </Box>        
+      </Container>      
+    </Router>
   );
 }
 
