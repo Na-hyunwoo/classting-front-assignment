06ee429e9b0f055a0ffdb5bf54d0d92706bf46e0
hw <dusdjeks@naver.com>
2023-01-13 18:25:59 +0900

    Delete remark

--- src/pages/ReviewNote.tsx
+++ src/pages/ReviewNote.tsx
@@ -5,14 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { COLORS } from "../styles";
 import { decode } from "html-entities";
 
-
-// 오답노트 기능이라고 하면.. 
-// result page에서 오답노트 버튼을 누르면 새로운 페이지로 향하는거지. 
-// 그러면 새로운 페이지에서는 여태까지 틀린 문제들을 저장하고 있는거야. 
-// 여기서 localStorage를 사용하는거지. 
-// 그래서 문제를 틀릴때마다, localStorage에 추가해서, 그걸 보여주는거임. 
-// 이렇게까지 구현하고 테스트코드 짜고 제출하면 될것같다. 
-
 interface ProblemType {
   question: string,
   selected: string,
