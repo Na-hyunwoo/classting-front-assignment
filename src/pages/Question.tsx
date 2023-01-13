8424cdc7da0d5a8fa59fe60ee4097b757e426e1e
hw <dusdjeks@naver.com>
2023-01-13 19:08:42 +0900

    Fix bug

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { ReactElement, useEffect, useState } from "react";
 import { Box } from "@mui/system";
